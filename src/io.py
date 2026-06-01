749775bdebe2ef90599a3d4c1a7fe960cf171acc
P. Dani <137707302+DaniD4n@users.noreply.github.com>
2026-06-01 14:16:35 -0300

    Add data loading function with normalization

--- src/io.py
+++ src/io.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
 
+def load_data(path):
+    df = pd.read_parquet(path)
+
+    w = df["fase"].to_numpy()
+    A = df["amplitude"].to_numpy()
+
+    A_norm = A / (np.max(np.abs(A)) + 1e-12)
+
+    return w, A, A_norm
