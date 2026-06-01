1775d71161b8ee2f52b0e3a89cd130b529f9b764
P. Dani <137707302+DaniD4n@users.noreply.github.com>
2026-06-01 15:43:14 -0300

    Refactor plotting functions to use add_sine

--- src/visualization.py
+++ src/visualization.py
@@ -1,37 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_sine(max_val):
-    x = np.linspace(0, 360, 500)
-    y = np.sin(np.deg2rad(x)) * max_val*1.1
-    plt.plot(x, y, alpha=0.3, label="Sine", color='red')
+
+def add_sine(ax, x, y_min, y_max):
+    sine = np.sin(np.deg2rad(x))
+    sine_scaled = (sine + 1) / 2
+    sine_scaled = sine_scaled * (y_max - y_min) + y_min
+    ax.plot(x, sine_scaled, color='red', alpha=0.3, label="AC reference")
+
 
 def plot_raw(w, A_norm):
-    plt.figure()
-    plt.scatter(w, A_norm, s=2, alpha=0.3)
-    plt.title("Raw PRPD")
-    plt.xlim(0, 360)
-    plt.ylim(-1, 1)
-    plot_sine(max(A_norm))
+    maximum = max(np.abs(A_norm))
+    fig, ax = plt.subplots()
+    ax.scatter(w, A_norm, s=2, alpha=0.3)
+    ax.set_title("Raw PRPD")
+    ax.set_xlim(0, 360)
+    ax.set_ylim(-maximum, maximum)
+
+    x = np.linspace(0, 360, 500)
+    add_sine(ax, x, -maximum, maximum)
+
 
 def plot_density(w, A_norm):
-    plt.figure()
-    plt.hist2d(w, A_norm, bins=200, cmap="inferno")
-    plt.colorbar()
-    plt.title("Density")
-    plot_sine(max(A_norm))
+    maximum = max(np.abs(A_norm))
+    fig, ax = plt.subplots()
+    ax.hist2d(w, A_norm, bins=200, cmap="inferno")
+    ax.set_title("Density")
+    
+    x = np.linspace(0, 360, 500)
+    add_sine(ax, x, -maximum, maximum)
+
 
 def plot_structure(x_grid, density, peaks):
-    plt.figure()
-    plt.plot(x_grid, density)
-    plt.scatter(x_grid[peaks], density[peaks], color="red")
-    plt.xlim(0, 360)
-    plot_sine(max(density))
+    maximum = max(density)
+    fig, ax = plt.subplots()
+    ax.plot(x_grid, density)
+    ax.scatter(x_grid[peaks], density[peaks], color="red")
+
+    ax.set_xlim(0, 360)
+    ax.set_ylim(0, maximum * 1.1)
+
+    x = np.linspace(0, 360, 500)
+    add_sine(ax, x, 0, maximum * 1.1)
+
 
 def plot_filter(w, A_norm, w_f, A_f):
-    plt.figure()
-    plt.scatter(w, A_norm, s=2, alpha=0.2, label="raw")
-    plt.scatter(w_f, A_f, s=2, label="filtered")
-    plt.legend()
-    plt.title("Filter result")
-    plot_sine(max(A_norm))
+    maximum = max(np.abs(A_norm))
+    fig, ax = plt.subplots()
+    ax.scatter(w, A_norm, s=2, alpha=0.2, label="raw")
+    ax.scatter(w_f, A_f, s=2, label="filtered")
+    ax.set_title("Filter result")
+    ax.legend()
+    ax.set_xlim(0, 360)
+    ax.set_ylim(-maximum, maximum)
+
+    x = np.linspace(0, 360, 500)
+    add_sine(ax, x, -maximum, maximum)
